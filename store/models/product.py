ca5364096286221a15b5a2eed7438a3034566de5
thelc127 <rasilamichhane@gmail.com>
2020-09-10 18:22:39 +0545

    categories

--- store/models/product.py
+++ store/models/product.py
@@ -1,9 +1,11 @@
 from django.db import models
+from .category import Category
 
 
 # Create your models here.
 class Product(models.Model):
         name = models.CharField(max_length=50)
         price = models.IntegerField(default=0)
+        category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
         description = models.CharField(max_length=20)
         image = models. ImageField(upload_to="uploads/products/")
