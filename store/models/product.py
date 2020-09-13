5f0e12173c3a8109fa79e10aae86e8234b31ae87
thelc127 <rasilamichhane@gmail.com>
2020-09-13 17:35:48 +0545

    front-end

--- store/models/product.py
+++ store/models/product.py
@@ -9,3 +9,8 @@ class Product(models.Model):
         category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
         description = models.CharField(max_length=20)
         image = models. ImageField(upload_to="uploads/products/")
+
+        @staticmethod
+        def get_all_products():
+            return Product.objects.all()
+
